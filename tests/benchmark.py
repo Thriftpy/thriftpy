46bb55a6a8c0d7696072330f9f9c98b952fd86b2
Lx Yu <i@lxyu.net>
2014-04-10 15:58:06 +0800

    more complicate object for benchmark

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -7,11 +7,22 @@ import addressbook_thrift as addressbook
 
 start = time.time()
 for i in range(10000):
-    n = addressbook.PhoneNumber()
-    n.type = 0
-    n.number = "123456"
-    b = serialize(n)
-    deserialize(addressbook.PhoneNumber(), b)
+    phone1 = addressbook.PhoneNumber()
+    phone1.type = addressbook.PhoneType.MOBILE
+    phone1.number = '555-1212'
+    phone2 = addressbook.PhoneNumber()
+    phone2.type = addressbook.PhoneType.HOME
+    phone2.number = '555-1234'
+    person = addressbook.Person()
+    person.name = "Alice"
+    person.phones = [phone1, phone2]
+    person.created_at = int(time.time())
+
+    ab = addressbook.AddressBook()
+    ab.people = {person.name: person}
+
+    binary = serialize(ab)
+    deserialize(addressbook.PhoneNumber(), binary)
 end = time.time()
 
 print(end - start)
