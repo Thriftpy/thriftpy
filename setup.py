65c829ca3628af25750f9d5103c25c310c6d71bf
Lx Yu <i@lxyu.net>
2014-04-12 17:47:09 +0800

    add setup.py file

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import setup, find_packages
 
-version = '0.1.0'
+version = "0.1.0"
 
-setup(
-    name='thriftpy',
-    version=version,
-    description='Thrift python lib reworked.',
-    keywords='thrift python thriftpy',
-    author='Lx Yu',
-    author_email='i@lxyu.net',
-    packages=find_packages(),
-    entry_points={},
-    url='http://lxyu.github.io/thriftpy/',
-    license="MIT",
-    zip_safe=False,
-    long_description=open('README.rst').read(),
-)
+install_requires = [
+    "pyparsing==2.0.1",
+]
+
+dev_requires = [
+    "flake8>=2.1.0",
+]
+
+setup(name="thriftpy",
+      version=version,
+      description="Pure python implemention of Apache Thrift.",
+      keywords="thrift python thriftpy",
+      author="Lx Yu",
+      author_email="i@lxyu.net",
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      entry_points={},
+      url="http://lxyu.github.io/thriftpy/",
+      license="MIT",
+      zip_safe=False,
+      long_description=open("README.rst").read(),
+      install_requires=install_requires,
+      extras_require={
+          "dev": dev_requires,
+      },
+      classifiers=[
+          "Topic :: Software Development"
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+      ])
