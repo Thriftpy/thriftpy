7c9186409ca857fd8bd53cdd2c04145eed7c801c
Lx Yu <i@lxyu.net>
2014-04-16 23:17:18 +0800

    use new binary protocol in benchmark.py

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,28 +1,29 @@
 import time
 
-from thriftpy.utils import serialize, deserialize
+from thriftpy.protocol import binary
 
 import addressbook_thrift as addressbook
+# import addressbook
 
 
 start = time.time()
 for i in range(10000):
     phone1 = addressbook.PhoneNumber()
     phone1.type = addressbook.PhoneType.MOBILE
-    phone1.number = '555-1212'
+    phone1.number = b'555-1212'
     phone2 = addressbook.PhoneNumber()
     phone2.type = addressbook.PhoneType.HOME
-    phone2.number = '555-1234'
+    phone2.number = b'555-1234'
     person = addressbook.Person()
-    person.name = "Alice"
+    person.name = b"Alice"
     person.phones = [phone1, phone2]
     person.created_at = int(time.time())
 
     ab = addressbook.AddressBook()
     ab.people = {person.name: person}
 
-    binary = serialize(ab)
-    deserialize(addressbook.PhoneNumber(), binary)
+    binary.write_output(binary.STRUCT, ab, ab.thrift_spec)
+
 end = time.time()
 
 print(end - start)
