252cf3d3ac59bd0bb2843719ad3aeef52899282b
Lx Yu <i@lxyu.net>
2014-04-17 15:13:31 +0800

    low level c memcpy instead of struct pack

--- thriftpy/protocol/binary.pyx
+++ thriftpy/protocol/binary.pyx
@@ -1,11 +1,20 @@
-import struct
+from cpython.array cimport array, clone
 
+from libc.string cimport memcmp, memcpy
 from libc.stdint cimport (
     int8_t,
     int16_t,
     int32_t,
     int64_t,
 )
+
+ctypedef fused num:
+    int8_t
+    int16_t
+    int32_t
+    int64_t
+    double
+
 
 cdef public enum TType:
     STOP = 0
@@ -31,61 +40,82 @@ cdef:
     int VERSION_MASK = 0xffff0000
     int VERSION_1 = 0x80010000
     int TYPE_MASK = 0x000000ff
+    array stringtemplate = array('B')
 
 
-cdef bytes write_int8(int8_t val):
-    return struct.pack("!b", val)
-
-cdef bytes write_i16(int16_t val):
-    return struct.pack("!h", val)
-
-cdef bytes write_i32(int32_t val):
-    return struct.pack("!i", val)
-
-cdef bytes write_i64(int64_t val):
-    return struct.pack("!q", val)
-
-cdef bytes write_bool(int8_t bool_):
-    if bool_:
-        return write_int8(1)
-    else:
-        return write_int8(0)
-
-cdef bytes write_double(double val):
-    return struct.pack("!d", val)
-
-cdef bytes write_string(bytes val):
+cdef void _revert_memcpy(char* outbuf, void* x, int sz):
     cdef:
-        int32_t length = len(val)
-        str fmt
-    fmt = "!i%ds" % length
-    return struct.pack(fmt, length, val)
+        int i
+        char* s = <char*> x
+
+    for i in range(sz):
+        outbuf[i] = s[sz-1-i]
+
+
+cdef bytes _write_num(num val):
+    cdef int32_t sz = sizeof(val)
+    cdef array output = clone(stringtemplate, sz, False)
+    _revert_memcpy(output.data.as_chars, &val, sz)
+    return output.data.as_chars[:sz]
+
+cpdef bytes write_i8(int8_t val):
+    return _write_num(val)
+
+cpdef bytes write_i16(int16_t val):
+    return _write_num(val)
+
+cpdef bytes write_i32(int32_t val):
+    return _write_num(val)
+
+cpdef bytes write_i64(int64_t val):
+    return _write_num(val)
+
+cpdef bytes write_bool(int8_t bool_):
+    if bool_:
+        return write_i8(1)
+    else:
+        return write_i8(0)
+
+cpdef bytes write_double(double val):
+    return _write_num(val)
+
+cpdef bytes write_string(bytes val):
+    cdef:
+        int32_t sz = sizeof(int32_t)
+        int32_t val_len = len(val)
+        array output = clone(stringtemplate, sz + val_len, True)
+    _revert_memcpy(output.data.as_chars, &val_len, sz)
+    memcpy(output.data.as_chars + sz, <char*>val, val_len)
+    return output.data.as_chars[:sz + val_len]
 
 cdef bytes write_field_begin(TType ttype, int32_t fid):
-    return struct.pack("!bh", ttype, fid)
+    return write_i8(ttype) + write_i32(fid)
 
 cdef bytes write_field_stop():
-    return struct.pack("!b", STOP)
+    return write_i8(STOP)
 
 cdef bytes write_list_begin(TType e_type, int32_t size):
-    return struct.pack("!bi", e_type, size)
+    return write_i8(e_type) + write_i32(size)
 
 cdef bytes write_map_begin(TType k_type, TType v_type, int32_t size):
-    return struct.pack("!bbi", k_type, v_type, size)
+    return write_i8(k_type) + write_i8(v_type) + write_i32(size)
 
 cpdef bytes write_output(TType ttype, val, spec=None):
     cdef:
         TType e_type, k_type, v_type
         int32_t i, val_len
-        bytes res
+        bytes res = b''
         tuple e_spec
         str e_name
 
     if ttype == BOOL:
-        return write_bool(val)
+        if val:
+            return write_i8(1)
+        else:
+            return write_i8(0)
 
     elif ttype == BYTE:
-        return write_int8(val)
+        return write_i8(val)
 
     elif ttype == DOUBLE:
         return write_double(val)
@@ -135,8 +165,6 @@ cpdef bytes write_output(TType ttype, val, spec=None):
         return res
 
     elif ttype == STRUCT:
-        res = b''
-
         for i in iter(spec):
             e_spec = spec[i]
             if len(e_spec) == 2:
@@ -155,8 +183,4 @@ cpdef bytes write_output(TType ttype, val, spec=None):
         return res
 
 cpdef bytes write_message_begin(bytes name, TType ttype, int32_t seqid):
-    cdef:
-        str fmt
-        int32_t name_len = len(name)
-    fmt = "!ii%dsi" % name_len
-    return struct.pack(fmt, VERSION_1 | ttype, name_len, name, seqid)
+    return write_string(name) + write_i8(ttype) + write_i32(seqid)
