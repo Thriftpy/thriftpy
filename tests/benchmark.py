aab683567a93065a05df31fe26eb8d3735e16988
Lx Yu <i@lxyu.net>
2014-04-17 14:28:01 +0800

    wrap tests in function

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -6,24 +6,31 @@ import addressbook_thrift as addressbook
 # import addressbook
 
 
-start = time.time()
-for i in range(10000):
-    phone1 = addressbook.PhoneNumber()
-    phone1.type = addressbook.PhoneType.MOBILE
-    phone1.number = b'555-1212'
-    phone2 = addressbook.PhoneNumber()
-    phone2.type = addressbook.PhoneType.HOME
-    phone2.number = b'555-1234'
-    person = addressbook.Person()
-    person.name = b"Alice"
-    person.phones = [phone1, phone2]
-    person.created_at = int(time.time())
+def encode(n):
+    for i in range(n):
+        phone1 = addressbook.PhoneNumber()
+        phone1.type = addressbook.PhoneType.MOBILE
+        phone1.number = b'555-1212'
+        phone2 = addressbook.PhoneNumber()
+        phone2.type = addressbook.PhoneType.HOME
+        phone2.number = b'555-1234'
+        person = addressbook.Person()
+        person.name = b"Alice"
+        person.phones = [phone1, phone2]
+        person.created_at = int(time.time())
 
-    ab = addressbook.AddressBook()
-    ab.people = {person.name: person}
+        ab = addressbook.AddressBook()
+        ab.people = {person.name: person}
 
-    binary.write_output(binary.STRUCT, ab, ab.thrift_spec)
+        binary.write_output(binary.STRUCT, ab, ab.thrift_spec)
 
-end = time.time()
 
-print(end - start)
+def main():
+    start = time.time()
+    encode(10000)
+    end = time.time()
+    print(end - start)
+
+
+if __name__ == "__main__":
+    main()
