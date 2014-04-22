82d57b677e1cc0544cb0053f62895d54fcbabde3
Lx Yu <i@lxyu.net>
2014-04-22 12:28:35 +0800

    add hexlify and hexprint helper

--- thriftpy/utils.py
+++ thriftpy/utils.py
@@ -14,3 +14,16 @@ def deserialize(thrift_object, buf, proto=TBinaryProtocol):
     protocol = proto(transport)
     thrift_object.read(protocol)
     return thrift_object
+
+
+def hexlify(byte_array, delimeter=' '):
+    return delimeter.join(map(lambda x: format(x, '02x'), byte_array))
+
+
+def hexprint(byte_array, delimeter=' ', count=10):
+    print("Bytes:")
+    print(byte_array)
+
+    print("\nHex:")
+    g = hexlify(byte_array, delimeter).split(delimeter)
+    print('\n'.join(' '.join(g[i:i+10]) for i in range(0, len(g), 10)))
