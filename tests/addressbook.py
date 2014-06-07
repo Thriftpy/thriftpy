a43c5281a7eff8deb532050db289ed3103ed61cb
Lx Yu <i@lxyu.net>
2014-06-07 15:17:35 +0800

    update addressbook.py demo, add default_spec.
    reflect the recent changes

--- tests/addressbook.py
+++ tests/addressbook.py
@@ -23,6 +23,7 @@ class PhoneNumber(TPayload):
         1: (TType.I32, "type"),
         2: (TType.STRING, "number"),
     }
+    default_spec = [("type", None), ("number", None)]
 
 
 class Person(TPayload):
@@ -31,6 +32,7 @@ class Person(TPayload):
         2: (TType.LIST, "phones", (TType.STRUCT, PhoneNumber)),
         4: (TType.I32, "created_at"),
     }
+    default_spec = [("name", None), ("phones", None), ("created_at", None)]
 
 
 class AddressBook(TPayload):
@@ -38,12 +40,14 @@ class AddressBook(TPayload):
         1: (TType.MAP, "people",
             (TType.STRING, (TType.STRUCT, Person)))
     }
+    default_spec = [("people", None)]
 
 
 class PersonNotExistsError(TException):
     thrift_spec = {
         1: (TType.STRING, "message")
     }
+    default_spec = [("message", None)]
 
 
 class AddressBookService(object):
@@ -58,55 +62,68 @@ class AddressBookService(object):
 
     class ping_args(TPayload):
         thrift_spec = {}
+        default_spec = []
 
     class ping_result(TPayload):
         thrift_spec = {}
+        default_spec = []
 
     class add_args(TPayload):
         thrift_spec = {
             1: (TType.STRUCT, "person", Person),
         }
+        default_spec = [("person", None)]
 
     class add_result(TPayload):
         thrift_spec = {
             0: (TType.BOOL, "success"),
         }
+        default_spec = [("success", None)]
 
     class remove_args(TPayload):
         thrift_spec = {
             1: (TType.STRING, "name"),
         }
+        default_spec = [("name", None)]
 
     class remove_result(TPayload):
         thrift_spec = {
             0: (TType.BOOL, "success"),
             1: (TType.STRUCT, "not_exists", PersonNotExistsError)
         }
+        default_spec = [("success", None), ("not_exists", None)]
 
     class get_args(TPayload):
         thrift_spec = {
             1: (TType.STRING, "name"),
         }
+        default_spec = [("name", None)]
 
     class get_result(TPayload):
         thrift_spec = {
             0: (TType.STRUCT, "success", Person),
+            1: (TType.STRUCT, "not_exists", PersonNotExistsError)
         }
+        default_spec = [("success", None), ("not_exists", None)]
 
     class book_args(TPayload):
         thrift_spec = {}
+        default_spec = []
 
     class book_result(TPayload):
         thrift_spec = {
             0: (TType.STRUCT, "success", AddressBook),
         }
+        default_spec = [("success", None)]
 
     class get_phones_args(TPayload):
         thrift_spec = {
             1: (TType.STRING, "name"),
         }
+        default_spec = [("name", None)]
 
     class get_phones_result(TPayload):
         thrift_spec = {
             0: (TType.MAP, "success", (TType.I32, TType.STRING)),
         }
+        default_spec = [("success", None)]
