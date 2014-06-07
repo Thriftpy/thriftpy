2ba84f52e4f3a63ffad610c431ebab76b2d09104
Lx Yu <i@lxyu.net>
2014-06-07 12:23:49 +0800

    revert to python binary in test_container.
    cython extension was not prefered in normal tests. cause c extension
    need re-compile after every changes, while python code do not.

--- tests/test_container.py
+++ tests/test_container.py
@@ -4,7 +4,6 @@ import thriftpy
 thriftpy.install_import_hook()
 
 from thriftpy.utils import serialize, deserialize
-from thriftpy.protocol import TCyBinaryProtocolFactory
 
 import container_thrift as container
 
@@ -14,8 +13,8 @@ def test_list_item():
     l_item.list_string = ['foo', 'bar']
     l_item.list_list_string = [['foo', 'bar']]
 
-    b = serialize(l_item, TCyBinaryProtocolFactory())
-    l_item2 = deserialize(container.ListItem(), b, TCyBinaryProtocolFactory())
+    b = serialize(l_item)
+    l_item2 = deserialize(container.ListItem(), b)
     assert l_item == l_item2
 
 
@@ -24,8 +23,8 @@ def test_map_item():
     m_item.map_string = {'foo': 'bar'}
     m_item.map_map_string = {'foo': {'hello': 'world'}}
 
-    b = serialize(m_item, TCyBinaryProtocolFactory())
-    m_item2 = deserialize(container.MapItem(), b, TCyBinaryProtocolFactory())
+    b = serialize(m_item)
+    m_item2 = deserialize(container.MapItem(), b)
     assert m_item == m_item2
 
 
@@ -34,8 +33,8 @@ def test_mix_item():
     x_item.list_map = [{'foo': 'bar'}]
     x_item.map_list = {'foo': ['hello', 'world']}
 
-    b = serialize(x_item, TCyBinaryProtocolFactory())
-    x_item2 = deserialize(container.MixItem(), b, TCyBinaryProtocolFactory())
+    b = serialize(x_item)
+    x_item2 = deserialize(container.MixItem(), b)
     assert x_item == x_item2
 
 
@@ -47,7 +46,6 @@ def test_list_struct():
     l_struct = container.ListStruct()
     l_struct.list_items = [l_item] * 100
 
-    b = serialize(l_struct, TCyBinaryProtocolFactory())
-    l_struct2 = deserialize(container.ListStruct(), b,
-                            TCyBinaryProtocolFactory())
+    b = serialize(l_struct)
+    l_struct2 = deserialize(container.ListStruct(), b)
     assert l_struct == l_struct2
