f24fd6a66d542790e2c5a24d1519e69ba051eab6
Daniel Blanchard <dblanchard@ets.org>
2014-06-18 09:03:05 -0400

    Reorder storm.py definitions to match storm.thrift and fix flake8 issues.

--- tests/storm.py
+++ tests/storm.py
@@ -11,11 +11,6 @@ from thriftpy.thrift import (
 )
 
 
-class TopologyInitialStatus(object):
-    ACTIVE = 1
-    INACTIVE = 2
-
-
 class JavaObjectArg(TPayload):
     thrift_spec = {1: (TType.I32, 'int_arg'),
                    2: (TType.I64, 'long_arg'),
@@ -25,21 +20,14 @@ class JavaObjectArg(TPayload):
                    6: (TType.DOUBLE, 'double_arg')}
 
 
-class ErrorInfo(TPayload):
-    thrift_spec = {1: (TType.STRING, 'error'),
-                   2: (TType.I32, 'error_time_secs')}
+class JavaObject(TPayload):
+    thrift_spec = {1: (TType.STRING, 'full_class_name'),
+                   2: (TType.LIST, 'args_list', (TType.STRUCT,
+                                                 JavaObjectArg))}
 
 
-class DRPCExecutionException(TException):
-    thrift_spec = {1: (TType.STRING, 'msg')}
-
-
-class SupervisorSummary(TPayload):
-    thrift_spec = {1: (TType.STRING, 'host'),
-                   2: (TType.I32, 'uptime_secs'),
-                   3: (TType.I32, 'num_workers'),
-                   4: (TType.I32, 'num_used_workers'),
-                   5: (TType.STRING, 'supervisor_id')}
+class NullStruct(TPayload):
+    thrift_spec = {}
 
 
 class GlobalStreamId(TPayload):
@@ -58,26 +46,20 @@ class Grouping(TPayload):
                    8: (TType.STRUCT, 'local_or_shuffle', NullStruct)}
 
 
-class NullStruct(TPayload):
-    thrift_spec = {}
-
-
-class Bolt(TPayload):
-    thrift_spec = {1: (TType.STRUCT, 'bolt_object', ComponentObject),
-                   2: (TType.STRUCT, 'common', ComponentCommon)}
-
-
-class InvalidTopologyException(TException):
-    thrift_spec = {1: (TType.STRING, 'msg')}
-
-
-class KillOptions(TPayload):
-    thrift_spec = {1: (TType.I32, 'wait_secs')}
-
-
 class StreamInfo(TPayload):
     thrift_spec = {1: (TType.LIST, 'output_fields', TType.STRING),
                    2: (2, 'direct')}
+
+
+class ShellComponent(TPayload):
+    thrift_spec = {1: (TType.STRING, 'execution_command'),
+                   2: (TType.STRING, 'script')}
+
+
+class ComponentObject(TPayload):
+    thrift_spec = {1: (TType.STRING, 'serialized_java'),
+                   2: (TType.STRUCT, 'shell', ShellComponent),
+                   3: (TType.STRUCT, 'java_object', JavaObject)}
 
 
 class ComponentCommon(TPayload):
@@ -89,24 +71,72 @@ class ComponentCommon(TPayload):
                    4: (TType.STRING, 'json_conf')}
 
 
-class ExecutorSpecificStats(TPayload):
-    thrift_spec = {1: (TType.STRUCT, 'bolt', BoltStats),
-                   2: (TType.STRUCT, 'spout', SpoutStats)}
+class SpoutSpec(TPayload):
+    thrift_spec = {1: (TType.STRUCT, 'spout_object', ComponentObject),
+                   2: (TType.STRUCT, 'common', ComponentCommon)}
 
 
-class DRPCRequest(TPayload):
-    thrift_spec = {1: (TType.STRING, 'func_args'),
-                   2: (TType.STRING, 'request_id')}
+class Bolt(TPayload):
+    thrift_spec = {1: (TType.STRUCT, 'bolt_object', ComponentObject),
+                   2: (TType.STRUCT, 'common', ComponentCommon)}
 
 
-class ComponentObject(TPayload):
-    thrift_spec = {1: (TType.STRING, 'serialized_java'),
-                   2: (TType.STRUCT, 'shell', ShellComponent),
-                   3: (TType.STRUCT, 'java_object', JavaObject)}
+class StateSpoutSpec(TPayload):
+    thrift_spec = {1: (TType.STRUCT, 'state_spout_object', ComponentObject),
+                   2: (TType.STRUCT, 'common', ComponentCommon)}
+
+
+class StormTopology(TPayload):
+    thrift_spec = {1: (TType.MAP, 'spouts', (TType.STRING, (TType.STRUCT,
+                                                            SpoutSpec))),
+                   2: (TType.MAP, 'bolts', (TType.STRING, (TType.STRUCT,
+                                                           Bolt))),
+                   3: (TType.MAP, 'state_spouts', (TType.STRING,
+                                                   (TType.STRUCT,
+                                                    StateSpoutSpec)))}
 
 
 class AlreadyAliveException(TException):
     thrift_spec = {1: (TType.STRING, 'msg')}
+
+
+class NotAliveException(TException):
+    thrift_spec = {1: (TType.STRING, 'msg')}
+
+
+class InvalidTopologyException(TException):
+    thrift_spec = {1: (TType.STRING, 'msg')}
+
+
+class TopologySummary(TPayload):
+    thrift_spec = {1: (TType.STRING, 'id'),
+                   2: (TType.STRING, 'name'),
+                   3: (TType.I32, 'num_tasks'),
+                   4: (TType.I32, 'num_executors'),
+                   5: (TType.I32, 'num_workers'),
+                   6: (TType.I32, 'uptime_secs'),
+                   7: (TType.STRING, 'status')}
+
+
+class SupervisorSummary(TPayload):
+    thrift_spec = {1: (TType.STRING, 'host'),
+                   2: (TType.I32, 'uptime_secs'),
+                   3: (TType.I32, 'num_workers'),
+                   4: (TType.I32, 'num_used_workers'),
+                   5: (TType.STRING, 'supervisor_id')}
+
+
+class ClusterSummary(TPayload):
+    thrift_spec = {1: (TType.LIST, 'supervisors', (TType.STRUCT,
+                                                   SupervisorSummary)),
+                   2: (TType.I32, 'nimbus_uptime_secs'),
+                   3: (TType.LIST, 'topologies', (TType.STRUCT,
+                                                  TopologySummary))}
+
+
+class ErrorInfo(TPayload):
+    thrift_spec = {1: (TType.STRING, 'error'),
+                   2: (TType.I32, 'error_time_secs')}
 
 
 class BoltStats(TPayload):
@@ -123,55 +153,15 @@ class BoltStats(TPayload):
                                                       ((TType.STRUCT,
                                                         GlobalStreamId),
                                                        TType.DOUBLE)))),
-                   4: (TType.MAP, 'executed', (TType.STRING, (TType.MAP,
-                                                              ((TType.STRUCT,
-                                                                GlobalStreamId),
-                                                               TType.I64)))),
+                   4: (TType.MAP, 'executed', (TType.STRING,
+                                               (TType.MAP, ((TType.STRUCT,
+                                                             GlobalStreamId),
+                                                            TType.I64)))),
                    5: (TType.MAP, 'execute_ms_avg', (TType.STRING,
                                                      (TType.MAP,
                                                       ((TType.STRUCT,
                                                         GlobalStreamId),
                                                        TType.DOUBLE))))}
-
-
-class ExecutorInfo(TPayload):
-    thrift_spec = {1: (TType.I32, 'task_start'),
-                   2: (TType.I32, 'task_end')}
-
-
-class ShellComponent(TPayload):
-    thrift_spec = {1: (TType.STRING, 'execution_command'),
-                   2: (TType.STRING, 'script')}
-
-
-class ExecutorStats(TPayload):
-    thrift_spec = {1: (TType.MAP, 'emitted', (TType.STRING, (TType.MAP,
-                                                             (TType.STRING,
-                                                              TType.I64)))),
-                   2: (TType.MAP, 'transferred', (TType.STRING, (TType.MAP,
-                                                                 (TType.STRING,
-                                                                  TType.I64)))),
-                   3: (TType.STRUCT, 'specific', ExecutorSpecificStats)}
-
-
-class NotAliveException(TException):
-    thrift_spec = {1: (TType.STRING, 'msg')}
-
-
-class RebalanceOptions(TPayload):
-    thrift_spec = {1: (TType.I32, 'wait_secs'),
-                   2: (TType.I32, 'num_workers'),
-                   3: (TType.MAP, 'num_executors', (TType.STRING, TType.I32))}
-
-
-class StormTopology(TPayload):
-    thrift_spec = {1: (TType.MAP, 'spouts', (TType.STRING, (TType.STRUCT,
-                                                            SpoutSpec))),
-                   2: (TType.MAP, 'bolts', (TType.STRING, (TType.STRUCT,
-                                                           Bolt))),
-                   3: (TType.MAP, 'state_spouts', (TType.STRING,
-                                                   (TType.STRUCT,
-                                                    StateSpoutSpec)))}
 
 
 class SpoutStats(TPayload):
@@ -187,29 +177,33 @@ class SpoutStats(TPayload):
                                                         TType.DOUBLE))))}
 
 
-class SubmitOptions(TPayload):
-    thrift_spec = {1: (TType.I32, 'initial_status')}
+class ExecutorSpecificStats(TPayload):
+    thrift_spec = {1: (TType.STRUCT, 'bolt', BoltStats),
+                   2: (TType.STRUCT, 'spout', SpoutStats)}
 
 
-class JavaObject(TPayload):
-    thrift_spec = {1: (TType.STRING, 'full_class_name'),
-                   2: (TType.LIST, 'args_list', (TType.STRUCT,
-                                                 JavaObjectArg))}
+class ExecutorStats(TPayload):
+    thrift_spec = {1: (TType.MAP, 'emitted', (TType.STRING, (TType.MAP,
+                                                             (TType.STRING,
+                                                              TType.I64)))),
+                   2: (TType.MAP, 'transferred', (TType.STRING,
+                                                  (TType.MAP, (TType.STRING,
+                                                               TType.I64)))),
+                   3: (TType.STRUCT, 'specific', ExecutorSpecificStats)}
 
 
-class SpoutSpec(TPayload):
-    thrift_spec = {1: (TType.STRUCT, 'spout_object', ComponentObject),
-                   2: (TType.STRUCT, 'common', ComponentCommon)}
+class ExecutorInfo(TPayload):
+    thrift_spec = {1: (TType.I32, 'task_start'),
+                   2: (TType.I32, 'task_end')}
 
 
-class TopologySummary(TPayload):
-    thrift_spec = {1: (TType.STRING, 'id'),
-                   2: (TType.STRING, 'name'),
-                   3: (TType.I32, 'num_tasks'),
-                   4: (TType.I32, 'num_executors'),
-                   5: (TType.I32, 'num_workers'),
-                   6: (TType.I32, 'uptime_secs'),
-                   7: (TType.STRING, 'status')}
+class ExecutorSummary(TPayload):
+    thrift_spec = {1: (TType.STRUCT, 'executor_info', ExecutorInfo),
+                   2: (TType.STRING, 'component_id'),
+                   3: (TType.STRING, 'host'),
+                   4: (TType.I32, 'port'),
+                   5: (TType.I32, 'uptime_secs'),
+                   7: (TType.STRUCT, 'stats', ExecutorStats)}
 
 
 class TopologyInfo(TPayload):
@@ -224,23 +218,39 @@ class TopologyInfo(TPayload):
                                                              ErrorInfo))))}
 
 
-class StateSpoutSpec(TPayload):
-    thrift_spec = {1: (TType.STRUCT, 'state_spout_object', ComponentObject),
-                   2: (TType.STRUCT, 'common', ComponentCommon)}
+class KillOptions(TPayload):
+    thrift_spec = {1: (TType.I32, 'wait_secs')}
 
 
-class ClusterSummary(TPayload):
-    thrift_spec = {1: (TType.LIST, 'supervisors', (TType.STRUCT,
-                                                   SupervisorSummary)),
-                   2: (TType.I32, 'nimbus_uptime_secs'),
-                   3: (TType.LIST, 'topologies', (TType.STRUCT,
-                                                  TopologySummary))}
+class RebalanceOptions(TPayload):
+    thrift_spec = {1: (TType.I32, 'wait_secs'),
+                   2: (TType.I32, 'num_workers'),
+                   3: (TType.MAP, 'num_executors', (TType.STRING, TType.I32))}
 
 
-class ExecutorSummary(TPayload):
-    thrift_spec = {1: (TType.STRUCT, 'executor_info', ExecutorInfo),
-                   2: (TType.STRING, 'component_id'),
-                   3: (TType.STRING, 'host'),
-                   4: (TType.I32, 'port'),
-                   5: (TType.I32, 'uptime_secs'),
-                   7: (TType.STRUCT, 'stats', ExecutorStats)}
+class TopologyInitialStatus(object):
+    ACTIVE = 1
+    INACTIVE = 2
+
+
+class SubmitOptions(TPayload):
+    thrift_spec = {1: (TType.I32, 'initial_status')}
+
+
+##### NIMBUS SERVICE
+
+
+class DRPCRequest(TPayload):
+    thrift_spec = {1: (TType.STRING, 'func_args'),
+                   2: (TType.STRING, 'request_id')}
+
+
+class DRPCExecutionException(TException):
+    thrift_spec = {1: (TType.STRING, 'msg')}
+
+
+
+#### DistributedRPC service
+
+
+#### DistributedRPCInvocations service
