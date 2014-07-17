5c80029edeae1f9abbd50984c3b3cb314b727aaf
lo <lodevil@live.cn>
2014-07-17 12:25:01 +0800

    port fix

--- thriftpy/protocol/cybin/endian_port.h
+++ thriftpy/protocol/cybin/endian_port.h
@@ -1,17 +1,6 @@
 
-#if defined(OS_MACOSX)
+#if defined(__APPLE__)
   #include <machine/endian.h>
-#elif defined(OS_SOLARIS)
-  #include <sys/isa_defs.h>
-  #ifdef _LITTLE_ENDIAN
-    #define LITTLE_ENDIAN
-  #else
-    #define BIG_ENDIAN
-  #endif
-#elif defined(OS_FREEBSD) || defined(OS_OPENBSD) || defined(OS_NETBSD) ||\
-      defined(OS_DRAGONFLYBSD)
-  #include <sys/types.h>
-  #include <sys/endian.h>
 #else
   #include <endian.h>
 #endif
