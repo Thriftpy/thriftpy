d015e819883ba663bd6dfe796d80157ba57dd5de
Lx Yu <i@lxyu.net>
2014-08-05 15:56:20 +0800

    add tests for load and load_module

--- tests/test_hook.py
+++ tests/test_hook.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import pickle
+import sys
+
+import pytest
+
 import thriftpy
 
 
@@ -18,3 +23,30 @@ def test_import_hook():
     import addressbook_thrift as ab_2
     print("Magic import succeed.")
     assert ab_2.DEFAULT_LIST_SIZE == 10
+
+
+def test_load():
+    ab_1 = thriftpy.load("addressbook.thrift")
+    ab_2 = thriftpy.load("addressbook.thrift",
+                         module_name="addressbook_thrift")
+
+    assert ab_1.__name__ == "addressbook"
+    assert ab_2.__name__ == "addressbook_thrift"
+
+    # load without module_name can't do pickle
+    with pytest.raises(pickle.PicklingError):
+        pickle.dumps(ab_1.Person(name='Bob'))
+
+    # load with module_name set and it can be pickled
+    person = ab_2.Person(name='Bob')
+    assert person == pickle.loads(pickle.dumps(person))
+
+
+def test_load_module():
+    ab = thriftpy.load_module("addressbook_thrift")
+    assert ab.__name__ == "addressbook_thrift"
+    assert sys.modules["addressbook_thrift"] == ab
+
+    # note we can import after load_module
+    import addressbook_thrift as ab2
+    assert ab2 == ab
