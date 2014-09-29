4fb6744eb617a78935562cfbb90bdf84007cfcc6
hit9 <nz2324@126.com>
2014-09-29 15:15:30 +0800

    add oneway example: sleep

---
+++ examples/oneway/sleep_server.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import time
+import thriftpy
+sleep_thrift = thriftpy.load("sleep.thrift", module_name="sleep_thrift")
+
+from thriftpy.rpc import make_server
+
+
+class Dispatcher(object):
+    def sleep(self, seconds):
+        print("I'm going to sleep %d seconds" % seconds)
+        time.sleep(seconds)
+        print("Sleep over!")
+
+
+def main():
+    server = make_server(sleep_thrift.Sleep, Dispatcher(),
+                         '127.0.0.1', 6000)
+    print("serving...")
+    server.serve()
+
+
+if __name__ == '__main__':
+    main()
