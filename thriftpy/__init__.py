b9d7dd8b70133db24b0f5904b282a534076189d4
maralla <maralla.ai@gmail.com>
2015-02-12 12:02:20 +0800

    tracker branch version upgrade to v0.1.15.tracking

--- thriftpy/__init__.py
+++ thriftpy/__init__.py
@@ -5,7 +5,7 @@ import sys
 from .hook import install_import_hook, remove_import_hook
 from .parser import load, load_module
 
-__version__ = '0.1.15'
+__version__ = '0.1.15.tracking'
 __python__ = sys.version_info
 
 __all__ = ["install_import_hook", "remove_import_hook",  "load", "load_module"]
