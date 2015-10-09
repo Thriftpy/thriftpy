e481e5d7d52af12a82432746da0aab08f531c977
Jon Parise <jon@indelible.org>
2015-10-09 12:00:28 -0700

    Support integer boolean constants.
    Some Thrift generators used to produce bad code for some languages that used
    'true' and 'false' as reserved keywords, so there are Thrift files in the wild
    that use {0,1} to represent the {false,true} boolean constant values.
    
    This is also supported by Apache Thrift.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -12,6 +12,8 @@ def test_comments():
 
 def test_constants():
     thrift = load('parser-cases/constants.thrift')
+    assert thrift.tbool is True
+    assert thrift.tboolint is True
     assert thrift.int16 == 3
     assert thrift.int32 == 800
     assert thrift.int64 == 123456789
