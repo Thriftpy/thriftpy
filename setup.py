47b2af047b541576f5a9d4e8dd1ade057eb473c3
maralla <maralla.ai@gmail.com>
2017-11-30 18:33:23 +0800

    Add python_requires

--- setup.py
+++ setup.py
@@ -79,6 +79,7 @@ setup(name="thriftpy",
       long_description=open("README.rst").read(),
       install_requires=install_requires,
       tests_require=tornado_requires,
+      python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
       extras_require={
           "dev": dev_requires,
           "tornado": tornado_requires
